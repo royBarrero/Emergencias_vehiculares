10417c20f8ba8c91805f549cbc822c659c790e9e
royBarrero <wigroy.barrero@gmail.com>
2026-04-13 16:49:15 -0400

    configuracion para render

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
